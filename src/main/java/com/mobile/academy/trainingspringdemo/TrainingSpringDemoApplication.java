3cedc946de447340900771f066b905e0ad537a07
cristian.iliescu <ciliescu@veridiumid.com>
2020-11-14 11:35:04 +0200

    start

--- src/main/java/com/mobile/academy/trainingspringdemo/TrainingSpringDemoApplication.java
+++ src/main/java/com/mobile/academy/trainingspringdemo/TrainingSpringDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TrainingSpringDemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TrainingSpringDemoApplication.class, args);
 	}
 
