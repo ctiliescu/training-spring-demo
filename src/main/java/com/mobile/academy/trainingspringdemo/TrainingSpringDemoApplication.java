a7456095b6b2624990abe1a331b9a21f2f1e6bf7
cristian.iliescu <ciliescu@veridiumid.com>
2020-11-14 11:17:05 +0200

    scheleton

---
+++ src/main/java/com/mobile/academy/trainingspringdemo/TrainingSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mobile.academy.trainingspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingSpringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mobile/academy/trainingspringdemo/TrainingSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobile.academy.trainingspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
