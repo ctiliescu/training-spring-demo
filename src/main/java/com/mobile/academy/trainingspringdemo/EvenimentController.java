a2b590d976df5354f3f1faa1ff9b08cdc587d981
aureliavoicu <aurelia.maria.voicu@gmail.com>
2020-11-14 14:46:16 +0200

    add EvenimentService.java

--- src/main/java/com/mobile/academy/trainingspringdemo/EvenimentController.java
+++ src/main/java/com/mobile/academy/trainingspringdemo/EvenimentController.java
@@ -1,5 +1,6 @@
 package com.mobile.academy.trainingspringdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -10,43 +11,16 @@ import java.util.stream.Collectors;
 @RequestMapping("/api/v1/evenimente")
 public class EvenimentController {
 
-    List<Eveniment> evenimenteAgregate = new ArrayList<>();
+    @Autowired
+    EvenimentService evenimentService;
 
     @GetMapping()
     public List<Eveniment> getEvenimente(@RequestParam(required = false) Integer evenimentId) {
-
-
-        if (evenimenteAgregate.isEmpty()) {
-            evenimenteAgregate.addAll(genericEvents());
-        }
-
-
-        if (evenimentId == null) {
-            return evenimenteAgregate;
-
-        } else {
-            return evenimenteAgregate.stream().filter(e ->
-                    e.getId() == evenimentId
-            ).collect(Collectors.toList());
-        }
-
+        return evenimentService.getListaEvenimente(evenimentId);
     }
 
     @PostMapping
     public void createEveniment(@RequestBody Eveniment eveniment) {
-        eveniment.generateID();
-        evenimenteAgregate.add(eveniment);
+        evenimentService.createEveniment(eveniment);
     }
-
-
-    public List<Eveniment> genericEvents() {
-        List<Eveniment> evenimente = new ArrayList<>();
-
-        evenimente.add(new Eveniment("Concert Metallica;asdasdasd;10/10/2020;10:00;11:59"));
-        evenimente.add(new Eveniment("Concert Fuego;impodobeste bradul;10/10/2020;12:00;23:00"));
-
-        return evenimente;
-    }
-
-
 }
---
+++ src/main/java/com/mobile/academy/trainingspringdemo/EvenimentService.java
@@ -0,0 +1,41 @@
+package com.mobile.academy.trainingspringdemo;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class EvenimentService {
+
+    List<Eveniment> listaEvenimente = new ArrayList<>();
+
+    public void createEveniment(Eveniment eveniment) {
+        eveniment.generateID();
+        listaEvenimente.add(eveniment);
+    }
+
+    public List<Eveniment> genericEvents() {
+        List<Eveniment> evenimente = new ArrayList<>();
+
+        evenimente.add(new Eveniment("Concert Metallica;asdasdasd;10/10/2020;10:00;11:59"));
+        evenimente.add(new Eveniment("Concert Fuego;impodobeste bradul;10/10/2020;12:00;23:00"));
+
+        return evenimente;
+    }
+
+    public List<Eveniment> getListaEvenimente(Integer evenimentId) {
+        if (listaEvenimente.isEmpty()) {
+            listaEvenimente.addAll(genericEvents());
+        }
+
+        if (evenimentId == null) {
+            return listaEvenimente;
+        } else {
+            return listaEvenimente.stream().filter(e ->
+                    e.getId() == evenimentId
+            ).collect(Collectors.toList());
+        }
+    }
+}
